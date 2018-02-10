81607a0c888f60d5e665971600887c8e3ba7831f
Net Salvage <toddsusser@outlook.com>
2018-02-10 17:07:32 -0500

    let's try this again

---
+++ SDFP/Assets/GameStartup.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement; //need this for SceneManager
+using UnityEngine;
+
+public class GameStartup : MonoBehaviour {
+    TestScript test;
+	// Use this for initialization
+	void Start () {
+         test = gameObject.AddComponent(typeof(TestScript)) as TestScript;
+        SceneManager.LoadScene("MenuMain", LoadSceneMode.Additive);
+        Debug.Log("New scene loaded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
