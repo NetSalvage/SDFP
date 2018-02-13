56acde9541c0f1c8dfcc0c489334019435a3fad2
Net Salvage <toddsusser@outlook.com>
2018-02-12 20:07:03 -0500

    Plans starting to form. Basics of grid-script organization started.

---
+++ SDFP/Assets/GridMgr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridMgr : MonoBehaviour {
+    /* PROBABLY?
+     * This script manages the grid.
+     * As rough as it looks, I should follow what I did on Dustin's project, effectively - instantiate prefabs as children, and the prefabs are composed of af ew things.
+     */
+
+    float cellWidth;
+    float spacer = 0.1f; //the space between cells
+    public int columns; //TODO: set otherwise
+    public int rows; //TODO: set otherwise
+
+    // Use this for initialization
+    void Start () {
+        
+        GridGen();
+	}
+
+    void GridGen()
+    {
+        //new Image  Component
+        
+    }
+}
